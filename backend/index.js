25a8717ca1df478b01b36d15a738f963c564fe57
= <=>
2025-03-04 10:53:31 +0530

    cors policies updated

--- backend/index.js
+++ backend/index.js
@@ -8,8 +8,10 @@ const { app, server } = require('./socket/index')
 
 // const app = express()
 app.use(cors({
-    origin : process.env.FRONTEND_URL,
-    credentials : true
+    origin: [process.env.FRONTEND_URL, "http://localhost:3000"], 
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"]
 }))
 app.use(express.json())
 app.use(cookiesParser())
