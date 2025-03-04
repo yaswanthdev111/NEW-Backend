4518d74bf88a10674bbee3381bbabbd42cae3113
= <=>
2025-03-04 11:17:10 +0530

    fixed cors pocilies

--- backend/index.js
+++ backend/index.js
@@ -6,26 +6,30 @@ const router = require('./routes/index')
 const cookiesParser = require('cookie-parser')
 const { app, server } = require('./socket/index')
 
-// const app = express()
-// app.use(cors({
-//     origin: [process.env.FRONTEND_URL, "http://localhost:3000", "https://livechat-frontend-ashen.vercel.app"], 
-//     credentials: true,
-//     methods: ["GET", "POST", "PUT", "DELETE"],
-//     allowedHeaders: ["Content-Type", "Authorization"],
-//     optionsSuccessStatus: 200,
-//     preflightContinue: false
-
-// }))
+const app = express()
+const allowedOrigins = [
+    process.env.FRONTEND_URL, 
+    "http://localhost:3000", 
+    "https://livechat-frontend-ashen.vercel.app"
+];
 
 app.use(cors({
-    origin: "*",
-    credentials: true
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
 console.log("Allowed Origins:", process.env.FRONTEND_URL);
 
-app.use(express.json())
-app.use(cookiesParser())
+app.use(express.json());
+app.use(cookiesParser());
 
 const PORT = process.env.PORT || 8080
 
