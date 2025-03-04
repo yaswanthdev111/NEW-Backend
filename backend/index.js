40d682ad736e5510f40e3a5de1175b2ee5c6336c
= <=>
2025-03-04 10:55:41 +0530

    cors added domains

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const { app, server } = require('./socket/index')
 
 // const app = express()
 app.use(cors({
-    origin: [process.env.FRONTEND_URL, "http://localhost:3000"], 
+    origin: [process.env.FRONTEND_URL, "http://localhost:3000","https://livechat-frontend-ashen.vercel.app"], 
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"]
